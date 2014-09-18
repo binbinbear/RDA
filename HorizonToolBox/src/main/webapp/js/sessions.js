4684c48418b7989c012ac044d35bef1fefebf6df
steng <steng@vmware.com>
2014-09-18 17:19:24 +0000

    add new chart view for sessions

--- HorizonToolBox/src/main/webapp/js/sessions.js
+++ HorizonToolBox/src/main/webapp/js/sessions.js
@@ -5,9 +5,19 @@ if (!ToolBox.Session || !ToolBox.Session.init){
 	ToolBox.Session = {
 			pools: [],
 			farms: [],
-			
+			history: [],
 			
 			refreshModel: function(){
+				$.ajax({
+					url: './session/concurrent',
+					type: "GET",
+					success: function (data) {
+						 ToolBox.Session.history = data.concurrentConnections;
+						 ToolBox.Session.updateChartView();
+						
+					}
+				}); 
+				
 				$.ajax({
 					url: './session/report',
 					type: "GET",
@@ -28,7 +38,72 @@ if (!ToolBox.Session || !ToolBox.Session.init){
 			},
 			
 			
+			updateChartView: function(){
+				if (!ToolBox.Session.history){
+					return;
+				}
+				var margin = {top: 20, right: 20, bottom: 30, left: 50},
+			    width = 960 - margin.left - margin.right,
+			    height = 500 - margin.top - margin.bottom;
 
+
+			var x = d3.time.scale()
+			    .range([0, width]);
+
+			var y = d3.scale.linear()
+			    .range([height, 0]);
+
+			var xAxis = d3.svg.axis()
+			    .scale(x)
+			    .orient("bottom");
+
+			var yAxis = d3.svg.axis()
+			    .scale(y)
+			    .orient("left");
+
+			var line = d3.svg.line()
+			    .x(function(d) { return x(d.date); })
+			    .y(function(d) { return y(d.concurrent); });
+
+			var svg = d3.select("svg")
+			    .attr("width", width + margin.left + margin.right)
+			    .attr("height", height + margin.top + margin.bottom)
+			  .append("g")
+			    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+			var data = ToolBox.Session.history;
+			data.forEach(function(d){
+
+			    d.date = new Date(d.date);
+			    d.concurrent = +d.concurrent;
+			  
+			});
+			
+			
+			 x.domain(d3.extent(data, function(d) { return d.date; }));
+			  y.domain(d3.extent(data, function(d) { return d.concurrent; }));
+
+			  svg.append("g")
+			      .attr("class", "x axis")
+			      .attr("transform", "translate(0," + height + ")")
+			      .call(xAxis);
+
+			  svg.append("g")
+			      .attr("class", "y axis")
+			      .call(yAxis)
+			    .append("text")
+			      .attr("transform", "rotate(-90)")
+			      .attr("y", 6)
+			      .attr("dy", ".71em")
+			      .style("text-anchor", "end")
+			      .text("Concurrent");
+
+			  svg.append("path")
+			      .datum(data)
+			      .attr("class", "line")
+			      .attr("d", line);
+				
+			},
 			
 			updateTableView: function(){
 				var table = $(".farmSessionTable");
