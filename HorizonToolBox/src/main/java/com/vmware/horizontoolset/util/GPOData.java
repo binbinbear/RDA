f2001759bebbf080ff62b0e88be193d59c2b9d8d
lxiaoyi <lxiaoyi@VMWARE-OB6THQJC.vmware.com>
2014-11-03 16:47:56 +0800

    Xiaoyi add Policies

--- HorizonToolBox/src/main/java/com/vmware/horizontoolset/util/GPOData.java
+++ HorizonToolBox/src/main/java/com/vmware/horizontoolset/util/GPOData.java
@@ -48,6 +48,377 @@ public class GPOData {
     
     public static final String TCPPORT_KEYNAME = "\"HKLM\\Software\\policies\\teradici\\pcoip\\pcoip_admin_defaults\"";
     public static final String TCPPORT_VALUENAME = "pcoip.tcpport";
+    
+    public static final String DEVICE_KEYNAME = "";
+    public static final String DEVICE_VALUENAME = "";
+    
+    public static final String POOL_KEYNAME = "";
+    public static final String POOL_VALUENAME = "";
+
+
+    public static final String PRODUCTIONLOGS_KEYNAME = "";
+    public static final String PRODUCTIONLOGS_VALUENAME = "";
+
+
+    public static final String DEBUGLOGS_KEYNAME = "";
+    public static final String DEBUGLOGS_VALUENAME = "";
+
+
+    public static final String LOGSIZE_KEYNAME = "";
+    public static final String LOGSIZE_VALUENAME = "";
+
+
+    public static final String LOGDIRECTORY_KEYNAME = "";
+    public static final String LOGDIRECTORY_VALUENAME = "";
+
+
+    public static final String SENDLOGS_KEYNAME = "";
+    public static final String SENDLOGS_VALUENAME = "";
+
+
+    public static final String INTERVAL_KEYNAME = "";
+    public static final String INTERVAL_VALUENAME = "";
+
+
+    public static final String OVERALLCPU_KEYNAME = "";
+    public static final String OVERALLCPU_VALUENAME = "";
+
+
+    public static final String OVERALLMEMORY_KEYNAME = "";
+    public static final String OVERALLMEMORY_VALUENAME = "";
+
+
+    public static final String PROCESSCPU_KEYNAME = "";
+    public static final String PROCESSCPU_VALUENAME = "";
+
+
+    public static final String PROCESSMEMORY_KEYNAME = "";
+    public static final String PROCESSMEMORY_VALUENAME = "";
+
+
+    public static final String PROCESSCHECK_KEYNAME = "";
+    public static final String PROCESSCHECK_VALUENAME = "";
+
+
+    public static final String CERTIFICATEREVOCATION_KEYNAME = "";
+    public static final String CERTIFICATEREVOCATION_VALUENAME = "";
+
+
+    public static final String CACHEDREVOCATION_KEYNAME = "";
+    public static final String CACHEDREVOCATION_VALUENAME = "";
+
+
+    public static final String CHECKTIMEOUT_KEYNAME = "";
+    public static final String CHECKTIMEOUT_VALUENAME = "";
+
+
+    public static final String LOSSLESS_KEYNAME = "";
+    public static final String LOSSLESS_VALUENAME = "";
+
+
+    public static final String MAXIMUM_KEYNAME = "";
+    public static final String MAXIMUM_VALUENAME = "";
+
+
+    public static final String MTU_KEYNAME = "";
+    public static final String MTU_VALUENAME = "";
+
+
+    public static final String FLOOR_KEYNAME = "";
+    public static final String FLOOR_VALUENAME = "";
+
+
+    public static final String ENDISAUDIO_KEYNAME = "";
+    public static final String ENDISAUDIO_VALUENAME = "";
+
+
+    public static final String LIMIT_KEYNAME = "";
+    public static final String LIMIT_VALUENAME = "";
+
+
+    public static final String SSL_KEYNAME = "";
+    public static final String SSL_VALUENAME = "";
+
+
+    public static final String ENCRYPTION_KEYNAME = "";
+    public static final String ENCRYPTION_VALUENAME = "";
+
+
+    public static final String USB_KEYNAME = "";
+    public static final String USB_VALUENAME = "";
+
+
+    public static final String TCP_KEYNAME = "";
+    public static final String TCP_VALUENAME = "";
+
+
+    public static final String UDP_KEYNAME = "";
+    public static final String UDP_VALUENAME = "";
+
+
+    public static final String CHANNELS_KEYNAME = "";
+    public static final String CHANNELS_VALUENAME = "";
+
+
+    public static final String IMAGE_KEYNAME = "";
+    public static final String IMAGE_VALUENAME = "";
+
+
+    public static final String FIPS_KEYNAME = "";
+    public static final String FIPS_VALUENAME = "";
+
+
+    public static final String VSPHERE_KEYNAME = "";
+    public static final String VSPHERE_VALUENAME = "";
+
+
+    public static final String SYNCHRONIZATION_KEYNAME = "";
+    public static final String SYNCHRONIZATION_VALUENAME = "";
+
+
+    public static final String ALTERNATE_KEYNAME = "";
+    public static final String ALTERNATE_VALUENAME = "";
+
+
+    public static final String CAD_KEYNAME = "";
+    public static final String CAD_VALUENAME = "";
+
+
+    public static final String TRANSPORT_KEYNAME = "";
+    public static final String TRANSPORT_VALUENAME = "";
+
+
+    public static final String VERBOSITY_KEYNAME = "";
+    public static final String VERBOSITY_VALUENAME = "";
+
+
+    public static final String TIMEINDAYS_KEYNAME = "";
+    public static final String TIMEINDAYS_VALUENAME = "";
+
+
+    public static final String SIZEINMB_KEYNAME = "";
+    public static final String SIZEINMB_VALUENAME = "";
+
+
+    public static final String EXCLUDE_KEYNAME = "";
+    public static final String EXCLUDE_VALUENAME = "";
+
+
+    public static final String SPLITDEVICE_KEYNAME = "";
+    public static final String SPLITDEVICE_VALUENAME = "";
+
+
+    public static final String OTHER_KEYNAME = "";
+    public static final String OTHER_VALUENAME = "";
+
+
+    public static final String HID_KEYNAME = "";
+    public static final String HID_VALUENAME = "";
+
+
+    public static final String INPUTDEVICES_KEYNAME = "";
+    public static final String INPUTDEVICES_VALUENAME = "";
+
+
+    public static final String OUTPUTDEVICES_KEYNAME = "";
+    public static final String OUTPUTDEVICES_VALUENAME = "";
+
+
+    public static final String KEYBOARD_KEYNAME = "";
+    public static final String KEYBOARD_VALUENAME = "";
+
+
+    public static final String VIDEODEVICES_KEYNAME = "";
+    public static final String VIDEODEVICES_VALUENAME = "";
+
+
+    public static final String SMARTCARDS_KEYNAME = "";
+    public static final String SMARTCARDS_VALUENAME = "";
+
+
+    public static final String AUTODEVICE_KEYNAME = "";
+    public static final String AUTODEVICE_VALUENAME = "";
+
+
+    public static final String EXCLUDEVP_KEYNAME = "";
+    public static final String EXCLUDEVP_VALUENAME = "";
+
+
+    public static final String INCLUDEVP_KEYNAME = "";
+    public static final String INCLUDEVP_VALUENAME = "";
+
+
+    public static final String EXCLUDEDF_KEYNAME = "";
+    public static final String EXCLUDEDF_VALUENAME = "";
+
+
+    public static final String INCLUDEDF_KEYNAME = "";
+    public static final String INCLUDEDF_VALUENAME = "";
+
+
+    public static final String EXCLLUDEALL_KEYNAME = "";
+    public static final String EXCLLUDEALL_VALUENAME = "";
+
+
+    public static final String MMR_KEYNAME = "";
+    public static final String MMR_VALUENAME = "";
+
+
+    public static final String MULTIMEDIA_KEYNAME = "";
+    public static final String MULTIMEDIA_VALUENAME = "";
+
+
+    public static final String DIRECTRDP_KEYNAME = "";
+    public static final String DIRECTRDP_VALUENAME = "";
+
+
+    public static final String SINGLESIGNON_KEYNAME = "";
+    public static final String SINGLESIGNON_VALUENAME = "";
+
+
+    public static final String TIMEOUT_KEYNAME = "";
+    public static final String TIMEOUT_VALUENAME = "";
+
+
+    public static final String CREDENTIALFILTER_KEYNAME = "";
+    public static final String CREDENTIALFILTER_VALUENAME = "";
+
+
+    public static final String USINGDNS_KEYNAME = "";
+    public static final String USINGDNS_VALUENAME = "";
+
+
+    public static final String DISABLETZNAME_KEYNAME = "";
+    public static final String DISABLETZNAME_VALUENAME = "";
+
+
+    public static final String TOGGLE_KEYNAME = "";
+    public static final String TOGGLE_VALUENAME = "";
+
+
+    public static final String ONCONNECT_KEYNAME = "";
+    public static final String ONCONNECT_VALUENAME = "";
+
+
+    public static final String ONRECONNECT_KEYNAME = "";
+    public static final String ONRECONNECT_VALUENAME = "";
+
+
+    public static final String ONDISCONNECT_KEYNAME = "";
+    public static final String ONDISCONNECT_VALUENAME = "";
+
+
+    public static final String SHOWICON_KEYNAME = "";
+    public static final String SHOWICON_VALUENAME = "";
+
+
+    public static final String FRAMEWORKCHANNEL_KEYNAME = "";
+    public static final String FRAMEWORKCHANNEL_VALUENAME = "";
+
+
+    public static final String UNITY_KEYNAME = "";
+    public static final String UNITY_VALUENAME = "";
+
+
+    public static final String MAXFRAMES_KEYNAME = "";
+    public static final String MAXFRAMES_VALUENAME = "";
+
+
+    public static final String MAXIMAGEHEIGHT_KEYNAME = "";
+    public static final String MAXIMAGEHEIGHT_VALUENAME = "";
+
+
+    public static final String MAXIMAGEWIDTH_KEYNAME = "";
+    public static final String MAXIMAGEWIDTH_VALUENAME = "";
+
+
+    public static final String DEFAULTIMAGEHEIGHT_KEYNAME = "";
+    public static final String DEFAULTIMAGEHEIGHT_VALUENAME = "";
+
+
+    public static final String DEFAULTIMAGEWIDTH_KEYNAME = "";
+    public static final String DEFAULTIMAGEWIDTH_VALUENAME = "";
+
+
+    public static final String DISABLERTAV_KEYNAME = "";
+    public static final String DISABLERTAV_VALUENAME = "";
+
+
+    public static final String PORTNUMBER_KEYNAME = "";
+    public static final String PORTNUMBER_VALUENAME = "";
+
+
+    public static final String SESSIONTIMEOUT_KEYNAME = "";
+    public static final String SESSIONTIMEOUT_VALUENAME = "";
+
+
+    public static final String DISCLAIMERENABLED_KEYNAME = "";
+    public static final String DISCLAIMERENABLED_VALUENAME = "";
+
+
+    public static final String DISCLAIMERTEXT_KEYNAME = "";
+    public static final String DISCLAIMERTEXT_VALUENAME = "";
+
+
+    public static final String APPLICTIONSENABLED_KEYNAME = "";
+    public static final String APPLICTIONSENABLED_VALUENAME = "";
+
+
+    public static final String AUTOCONNECT_KEYNAME = "";
+    public static final String AUTOCONNECT_VALUENAME = "";
+
+
+    public static final String ALWAYSCONNECT_KEYNAME = "";
+    public static final String ALWAYSCONNECT_VALUENAME = "";
+
+
+    public static final String SCREENSIZE_KEYNAME = "";
+    public static final String SCREENSIZE_VALUENAME = "";
+
+
+    public static final String PCOIPPORT_KEYNAME = "";
+    public static final String PCOIPPORT_VALUENAME = "";
+
+
+    public static final String RDPPORT_KEYNAME = "";
+    public static final String RDPPORT_VALUENAME = "";
+
+
+    public static final String BLASTPORT_KEYNAME = "";
+    public static final String BLASTPORT_VALUENAME = "";
+
+
+    public static final String IPADDRESS_KEYNAME = "";
+    public static final String IPADDRESS_VALUENAME = "";
+
+
+    public static final String CHANNELPORT_KEYNAME = "";
+    public static final String CHANNELPORT_VALUENAME = "";
+
+
+    public static final String USBENABLED_KEYNAME = "";
+    public static final String USBENABLED_VALUENAME = "";
+
+
+    public static final String MMRENABLED_KEYNAME = "";
+    public static final String MMRENABLED_VALUENAME = "";
+
+
+    public static final String RESETENABLED_KEYNAME = "";
+    public static final String RESETENABLED_VALUENAME = "";
+
+
+    public static final String USBAUTOCONNECT_KEYNAME = "";
+    public static final String USBAUTOCONNECT_VALUENAME = "";
+
+
+    public static final String CACHETIMEOUT_KEYNAME = "";
+    public static final String CACHETIMEOUT_VALUENAME = "";
+
+
+    public static final String DISSESSIONTIMEOUT_KEYNAME = "";
+    public static final String DISSESSIONTIMEOUT_VALUENAME = "";
+    
     /***
      * \"HKLM\Software\policies\teradici\pcoip\pcoip_admin_defaults\", valuename=pcoip.max_link_rate
      * \"HKLM\Software\policies\teradici\pcoip\pcoip_admin_defaults\", valuename=pcoip.mtu_size, value=1000
--- HorizonToolBox/src/main/webapp/js/policy.js
+++ HorizonToolBox/src/main/webapp/js/policy.js
@@ -31,12 +31,87 @@ if (!ToolBox.Policy || !ToolBox.Policy.init){
 			},
 
 			updatePlicies: function (){
+				$( "#message" ).empty();
+		    	$("#message").append("<div class=\"loadingdiv\"></div>");
+		    	
 				$.ajax({
 					url: './policy/updatepolicies',
 					type: "GET",
-					data:{pool:$("#desktoppool").val(),clipboard:$("#clipboard").val()},
-					success: function (data) {
-						
+					//做一个判断，传有值得参数。for e: if $("#desktoppool").val() == "" | $("#desktoppool").val().equal(null)),不加入data
+					data:{pool:$("#desktoppool").val(), clipboard:$("#clipboard").val(), 
+						  device:$("#device").val(), productionLogs:$("#productionLogs").val(), 
+						  debugLogs:$("#debugLogs").val(), logSize:$("#logSize").val(), 
+						  logDirectory:$("#logDirectory").val(), 
+						  sendLogs:$("#sendLogs").val(), interval:$("#interval").val(),
+						  overallCPU:$("#overallCPU").val(), overallMemory:$("#overallMemory").val(),
+						  processCPU:$("#processCPU").val(), processMemory:$("#processMemory").val(),
+						  processCheck:$("#processCheck").val(), certificateRevocation:$("#certificateRevocation").val(),
+						  cachedRevocation:$("#cachedRevocation").val(), checkTimeout:$("#checkTimeout").val(),
+						  //
+						  lossless:$("#lossless").val(), maximum:$("#maximum").val(),
+						  MTU:$("#MTU").val(), floor:$("#floor").val(),
+						  enDisAudio:$("#enDisAudio").val(), limit:$("#limit").val(),
+						  SSL:$("#SSL").val(), encryption:$("#encryption").val(),
+						  USB:$("#USB").val(), TCP:$("#TCP").val(),
+						  UDP:$("#UDP").val(), channels:$("#channels").val(),
+						  image:$("#image").val(), FIPS:$("#FIPS").val(),
+						  vSphere:$("#vSphere").val(), synchronization:$("#synchronization").val(),
+						  alternate:$("#alternate").val(), CAD:$("#CAD").val(),
+						  transport:$("#transport").val(), verbosity:$("#verbosity").val(),
+						  timeindays:$("#timeindays").val(), sizeinmb:$("#sizeinmb").val(),
+						  //
+						  exclude:$("#exclude").val(), splitdevice:$("#splitdevice").val(),
+						  other:$("#other").val(), HID:$("#HID").val(),
+						  inputdevices:$("#inputdevices").val(), outputdevices:$("#outputdevices").val(),
+						  keyboard:$("#keyboard").val(), videodevices:$("#videodevices").val(),
+						  smartcards:$("#smartcards").val(), autodevice:$("#autodevice").val(),
+						  excludeVP:$("#excludeVP").val(), includeVP:$("#includeVP").val(),
+						  excludeDF:$("#excludeDF").val(), includeDF:$("#includeDF").val(),
+						  exclludeAll:$("#exclludeAll").val(), MMR:$("#MMR").val(),
+						  multimedia:$("#multimedia").val(), directRDP:$("#directRDP").val(),
+						  singleSignon:$("#singleSignon").val(), timeout:$("#timeout").val(),
+						  credentialFilter:$("#credentialFilter").val(), usingDNS:$("#usingDNS").val(),
+						  disableTZName:$("#disableTZName").val(), toggle:$("#toggle").val(),
+						  onConnect:$("#onConnect").val(), onReconnect:$("#onReconnect").val(),
+						  onDisconnect:$("#onDisconnect").val(), showIcon:$("#showIcon").val(),
+						  frameworkChannel:$("#frameworkChannel").val(), unity:$("#unity").val(),
+						  maxFrames:$("#maxFrames").val(), maxImageHeight:$("#maxImageHeight").val(),
+						  maxImageWidth:$("#maxImageWidth").val(), defaultImageHeight:$("#defaultImageHeight").val(),
+						  defaultImageWidth:$("#defaultImageWidth").val(), disableRTAV:$("#disableRTAV").val(),
+						  portNumber:$("#portNumber").val(), sessionTimeout:$("#sessionTimeout").val(),
+						  disclaimerEnabled:$("#disclaimerEnabled").val(), disclaimerText:$("#disclaimerText").val(),
+						  applictionsEnabled:$("#applictionsEnabled").val(), autoConnect:$("#autoConnect").val(),
+						  alwaysConnect:$("#alwaysConnect").val(), screenSize:$("#screenSize").val(),
+						  PCoIPport:$("#PCoIPport").val(), RDPport:$("#RDPport").val(),
+						  blastPort:$("#blastPort").val(), IPaddress:$("#IPaddress").val(),
+						  channelPort:$("#channelPort").val(), USBenabled:$("#USBenabled").val(),
+						  MMRenabled:$("#MMRenabled").val(), resetEnabled:$("#resetEnabled").val(),
+						  USBautoConnect:$("#USBautoConnect").val(), cacheTimeout:$("#cacheTimeout").val(),
+						  DisSessionTimeout:$("#DisSessionTimeout").val() },
+					success: function (data) {						
+						var loadingDiv = $(".loadingdiv");
+				    	loadingDiv.removeClass("loadingdiv");
+				    	$(function(){
+							var tr="<div class='ui-state-highlight ui-corner-all' style='padding: 0 .7em; display:none;'><p><span class='ui-icon ui-icon-info' style='float: left; margin-right: .3em;'></span>Successful !</p></div>";
+							$( "#message" ).empty();
+							$( "#message" ).append( tr );
+							$("div.ui-state-highlight").slideDown(1500,function(){setTimeout(function(){$("div.ui-state-highlight").slideUp(1500);},"2000");});
+							//setTimeout("$( '#message' ).empty();",2000);
+							//$("div.ui-state-highlight").slideDown(1000,function(){$("div.ui-state-highlight").slideUp(2000);});
+							//$("div.ui-state-highlight").slideUp(2000);
+						});
+				    	
+					},
+					error: function (){
+						var loadingDiv = $(".loadingdiv");
+				    	loadingDiv.removeClass("loadingdiv");
+				    	$(function(){
+							var tr="<div class='ui-state-error ui-corner-all' style='padding: 0 .7em; display:none;'><p><span class='ui-icon ui-icon-alert' style='float: left; margin-right: .3em;'></span>ERROR !</p></div>";
+							$( "#message" ).empty();
+							$( "#message" ).append( tr );
+							//setTimeout("$( '#message' ).empty();",2000);
+							$("div.ui-state-error").slideDown(1500,function(){setTimeout(function(){$("div.ui-state-error").slideUp(1500);},"2000");});
+						});
 					}
 				}); 
 			}
