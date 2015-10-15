d9efe3a9ec9b1db48c6013a77bfb6fc3f6f70d43
nanw <nanw@vmware.com>
2015-10-15 23:24:20 +0800

    ra - log location/version display/help update
    ra - log location/version display/help update
    
    Bug Number: 0

--- lab/HorizonRemoteAssistance/HorizonRemoteAssistance/HRAInstaller/Form1.Designer.cs
+++ lab/HorizonRemoteAssistance/HorizonRemoteAssistance/HRAInstaller/Form1.Designer.cs
@@ -127,10 +127,10 @@
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Location = new System.Drawing.Point(15, 336);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(189, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(81, 13);
             this.linkLabel1.TabIndex = 11;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Version: 0.4 Beta, For Horizon Toolbox";
+            this.linkLabel1.Text = "Version: 1.0.3.4";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // linkLabel2
