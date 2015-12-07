867f86f61b0e078f663dfd034e3703355b3041c2
steng <steng@vmware.com>
2015-12-07 21:09:37 +0800

    fix a bug for clients auditing

--- HorizonToolBox/src/main/java/com/vmware/horizontoolset/Application.java
+++ HorizonToolBox/src/main/java/com/vmware/horizontoolset/Application.java
@@ -12,31 +12,32 @@ import com.vmware.horizontoolset.util.SessionUtil;
 public class Application {
 	private static Logger log = Logger.getLogger(Application.class);
 
-    
+
     public static final String MAINPAGE = "toolset";
-    
+
     public Application(){
     	log.info("Horizon Tool Set Application is being loaded!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
     public void setViewServerPath(String viewServerPath){
     	String sysDriver = System.getProperty("user.home");
 		sysDriver = sysDriver.substring(0, sysDriver.indexOf(":"));
-		LDAP.setViewServerPath(sysDriver + viewServerPath.substring(viewServerPath.indexOf(":")-1));
+		LDAP.setViewServerPath(sysDriver + viewServerPath.substring(viewServerPath.indexOf(":")));
     	//LDAP.setViewServerPath(viewServerPath);
     }
-    
+
 	public void setEventCachedDays(int days){
 		EventDBCache.setCachedDays(days);
 	}
-	
+
 	public void setEventPagingSize(int size){
 		EventDBCache.setPagingSize(size);
 	}
-	
-    
+
+
 	public void init(){
 		Thread t = new Thread(new Runnable(){
-		    public void run(){
+		    @Override
+			public void run(){
 				log.info("UPdating event db cache!");
 				EventDBCache.updateCache();
 				log.info("Updated event db cache!");
@@ -44,18 +45,18 @@ public class Application {
 		});
 		t.start();
 	}
-	
+
 	public void setMaximumSessions(String maximumSessions){
 		int maxSession = Integer.parseInt(maximumSessions);
 		log.info("maximumSessions is set to be:"+maxSession);
 		SessionUtil.setMaximumSessions(maxSession);
 	}
-	
-	
+
+
 	public void setConnTimeout(int connTimeout){
 		log.info("connectionTimeout is set to be:"+connTimeout);
 		ReportUtil.setConnectionTimeout(connTimeout);
 	}
-	
-	
+
+
 }
--- HorizonToolBox/src/main/java/com/vmware/horizontoolset/viewapi/operator/DesktopPool.java
+++ HorizonToolBox/src/main/java/com/vmware/horizontoolset/viewapi/operator/DesktopPool.java
@@ -2,8 +2,6 @@ package com.vmware.horizontoolset.viewapi.operator;
 
 import java.util.List;
 
-import com.vmware.horizontoolset.console.VMServiceImplVCenter;
-import com.vmware.horizontoolset.util.SessionUtil;
 import com.vmware.vdi.vlsi.binding.vdi.entity.DesktopId;
 import com.vmware.vdi.vlsi.binding.vdi.entity.SessionId;
 import com.vmware.vdi.vlsi.binding.vdi.resources.Desktop;
@@ -27,12 +25,13 @@ public class DesktopPool {
 	private DesktopSummaryData summary;
 	public final DesktopId id;
 	private DesktopInfo info;
-	
+
 	public final CachedObjs<Machine> machines = new CachedObjs<Machine>(ViewConfig.SAFE_API_TIMEOUT) {
-				
+
 		@Override
 		protected void populateCache(List<Machine> objects) {
-			QueryFilter filter = QueryFilter.equals(MachineSummaryViewCName.MACHINE_SUMMARY_VIEW_CNAME.base.desktop, id); 
+			objects.clear();
+			QueryFilter filter = QueryFilter.equals(MachineSummaryViewCName.MACHINE_SUMMARY_VIEW_CNAME.base.desktop, id);
 	        try (Query<MachineSummaryView> query = new Query<>(conn, MachineSummaryView.class, filter)) {
 	            for (MachineSummaryView v : query) {
 	            	objects.add(new Machine(conn, v, summary.name));
@@ -40,12 +39,13 @@ public class DesktopPool {
 	        }
 		}
 	};
-	
+
 	public final CachedObjs<String> entitledUsers = new CachedObjs<String>() {
-				
+
 		@Override
 		protected void populateCache(List<String> objects) {
-			QueryFilter filter = QueryFilter.contains(EntitledUserOrGroupLocalSummaryViewCName.ENTITLED_USER_OR_GROUP_LOCAL_SUMMARY_VIEW_CNAME.localData.desktops, new DesktopId[] {id}); 
+			objects.clear();
+			QueryFilter filter = QueryFilter.contains(EntitledUserOrGroupLocalSummaryViewCName.ENTITLED_USER_OR_GROUP_LOCAL_SUMMARY_VIEW_CNAME.localData.desktops, new DesktopId[] {id});
 	        try (Query<EntitledUserOrGroupLocalSummaryView> query = new Query<>(conn, EntitledUserOrGroupLocalSummaryView.class, filter)) {
 	            for (EntitledUserOrGroupLocalSummaryView v : query) {
 	            	objects.add(v.base.loginName);
@@ -53,12 +53,13 @@ public class DesktopPool {
 	        }
 		}
 	};
-	
+
 	public final CachedObjs<Session> sessions = new CachedObjs<Session>(ViewConfig.SAFE_API_TIMEOUT) {
-		
+
 		@Override
 		protected void populateCache(List<Session> objects) {
-			QueryFilter filter = QueryFilter.equals(SessionLocalSummaryViewCName.SESSION_LOCAL_SUMMARY_VIEW_CNAME.referenceData.desktop, id); 
+			objects.clear();
+			QueryFilter filter = QueryFilter.equals(SessionLocalSummaryViewCName.SESSION_LOCAL_SUMMARY_VIEW_CNAME.referenceData.desktop, id);
 	        try (Query<SessionLocalSummaryView> query = new Query<>(conn, SessionLocalSummaryView.class, filter)) {
 	            for (SessionLocalSummaryView v : query) {
 	            	objects.add(new Session(conn, v));
@@ -66,13 +67,13 @@ public class DesktopPool {
 	        }
 		}
 	};
-	
+
 	DesktopPool(Connection conn, DesktopSummaryView dsv) {
 		this.conn = conn;
 		this.id = dsv.id;
 		this.summary = dsv.desktopSummaryData;
 	}
-	
+
 	public DesktopPool(Connection conn, DesktopId id) {
 		this.conn = conn;
 		this.id = id;
@@ -85,7 +86,7 @@ public class DesktopPool {
 		}
 		return info;
 	}
-	
+
 	public DesktopSummaryData getSummary() {
 		if (summary == null) {
 			Desktop service = conn.get(Desktop.class);
@@ -93,36 +94,36 @@ public class DesktopPool {
 		}
 		return summary;
 	}
-	
+
 	public void dump() {
-				
+
 		System.out.println("------------");
 		System.out.println(getSummary().displayName + ": " + getInfo().type);
-		
+
 		DesktopInfo info = getInfo();
-		
+
 		UserAssignment ua;
 		if (info.manualDesktopData != null) {
 			ua = info.manualDesktopData.userAssignment;
 			System.out.println(ua.userAssignment);
 		}
-		
+
 		if (info.automatedDesktopData != null) {
 			ua = info.automatedDesktopData.userAssignment;
 			System.out.println(ua.userAssignment);
 		}
-		
+
 		if (info.rdsDesktopData != null) {
 			//FarmId farmId = info.rdsDesktopData.farm;
 			//System.out.println("Farm ID: " + farmId);
 		}
 
-		
+
 //		GlobalEntitlementId eid = info.globalEntitlementData.globalEntitlement;
-//		
+//
 //		GlobalEntitlement globalEntitlement = op.getConnection().get(GlobalEntitlement.class);
 //		GlobalEntitlementInfo gEntInfo = globalEntitlement.get(eid);
-		
+
 
 	}
 
@@ -130,13 +131,13 @@ public class DesktopPool {
 		public int notInUse;
 		public int inUse;
 	}
-	
+
 	public MachineStatistics getMachineStat(boolean forceRefresh) {
-					
+
 		List<Machine> machines = this.machines.get(forceRefresh);
-		
+
 		MachineStatistics ret = new MachineStatistics();
-		
+
 		for (Machine m : machines) {
 			SessionId sid = m.summaryView.base.session;
 			if (sid == null) {
@@ -146,13 +147,13 @@ public class DesktopPool {
 				/*
 				com.vmware.vdi.vlsi.binding.vdi.users.Session sessionService = va.op.getConnection().get(com.vmware.vdi.vlsi.binding.vdi.users.Session.class);
 				SessionLocalSummaryView v = sessionService.getLocalSummaryView(sid);
-				
+
 				System.out.println(m.getSummaryView().base.name + ": " + v.namesData.userName);
 				*/
 			}
 		}
-		
+
 		return ret;
 	}
-	
+
 }
